f0e0f546b3a273f4f6e3cb208414c9ed819e7e36
hiik3n <hiik3n@gmail.com>
2016-10-19 20:12:13 +0700

    minestone2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,21 @@ class UsersController < ApplicationController
   def index
     @users = User.all.shuffle
   end
+
+  def create
+		@user = User.new user_params
+	  if @user.save
+	    flash[:success] = "Account created!"
+	    session[:user_id] = @user.id
+	    redirect_to users_path # create user, let user in and redirect to /users
+	  else
+	    flash[:error] = "Error: #{@user.errors.full_messages.to_sentence}"
+	    redirect_to new_user_path
+	  end
+  end
+
+  def user_params
+  	params.permit(:user).require(:name, :email, :password, :password_confirmation)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root 'welcome#index'
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/login', to:'sessions#new'
+
 end
